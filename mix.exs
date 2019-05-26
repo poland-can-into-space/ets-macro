f06e9052a2777773e04bf88622373642fbcf8123
poland-can-into-space <40441297+poland-can-into-space@users.noreply.github.com>
2019-05-26 16:41:55 +0900

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,10 +4,13 @@ defmodule EtsMacro.MixProject do
   def project do
     [
       app: :ets_macro,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "You can define your ets module with one macro call.",
+      package: package(),
+      deps: deps(),
     ]
   end
 
@@ -21,8 +24,19 @@ defmodule EtsMacro.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      name: :ets_macro,
+      source_url: "https://github.com/poland-can-into-space/ets-macro",
+      licenses: ["MIT"],
+      maintainers: ["Thomas Cameron"],
+      links: %{"GitHub" => "https://github.com/poland-can-into-space/ets-macro"}
     ]
   end
 end
